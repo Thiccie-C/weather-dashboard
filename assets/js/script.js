07ac9bfa2e19785c12c35f4678f32c976d688f8a
Thiccie-C <Nelsonnoremac@gmail.com>
2022-01-09 18:25:20 -0600

    Added History Functionality, Added current weather functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var citySearchInput = document.querySelector("#city")
 var citySearchForm = document.querySelector("#city-search")
 var todaysWeatherContainerEl = document.querySelector("#today")
 var citySearchName = document.querySelector("#city-name")
-
+var historyDiv = document.querySelector("#history")
 
 
 var formSubmitHandler = function(event) {
@@ -10,58 +10,84 @@ var formSubmitHandler = function(event) {
     var city = citySearchInput.value.trim()
     if (city) {
         citySearchInput.value="";
-        console.log(city)
-        getWeather(city)
+        getCityName(city)
     }
     else {
         alert("Please Enter a city")
     }
 }
-var getWeather = function(data){
-var apiUrl="https://api.openweathermap.org/data/2.5/weather?q=" + data + ",us&APPID=cec0e7937cc29a0b1318e2f303f485a2"
+var getCityName = function(data){
+var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + data + ",us&APPID=cec0e7937cc29a0b1318e2f303f485a2"
+  fetch(cityUrl).then(function(response){
+    response.json().then(function(data){
+      var lon = data.coord.lon
+      var lat = data.coord.lat
+      var lonRounded = Math.round(lon * 10) / 10
+      var latRounded = Math.round(lat * 10) /10
+      lon = lonRounded
+      lat = latRounded 
+      citySearchName.textContent = data.name;
+      var historyButtonEl = document.createElement("button")
+      historyButtonEl.className = "history-contents " + data.name
+      historyButtonEl.textContent = data.name
+      historyDiv.appendChild(historyButtonEl)
+      historyButtonEl.onclick = function(){
+        historyInfo(data.name)
+      }
+      getWeather(lon, lat)
+    })
+  })
+  
+}
+var getWeather = function(lon, lat){
+var apiUrl="https://api.openweathermap.org/data/2.5/onecall?lat=" +lat + "&lon=" + lon + "&appid=cec0e7937cc29a0b1318e2f303f485a2"
 fetch(apiUrl).then(function(response) {
-  console.log(response);
   response.json().then(function(data) {
     displayWeather(data, data)
+    
   });
 });
 };
 var displayWeather = function(data, city) {
-
-  console.log(data.main.temp)
-
-  console.log(city.name)
-
   todaysWeatherContainerEl.textContent= "";
 
-  citySearchName.textContent = city.name;
 
   var todayEl = document.createElement("div");
-  todayEl.addClass = " "
+  todayEl.className = "today-contents"
 
-  var cityEl = document.createElement("span");
-  var todayTempEl = document.createElement("span");
-  var todayWindEl = document.createElement("span");
-  var todayHumidityEl = document.createElement("span")
-  var todayUVIndexEl = document.createElement("span")
+  var todayIcon = document.createElement("img")
+  var todayTempEl = document.createElement("div");
+  var todayWindEl = document.createElement("div");
+  var todayHumidityEl = document.createElement("div")
+  var todayUVIndexEl = document.createElement("div")
+  
+  todayTempEl.textContent = "Temp: " + data.current.temp + "%"
+  todayWindEl.textContent = "Wind: " + data.current.wind_speed + "MPH"
+  todayHumidityEl.textContent = "Humidity: " + data.current.humidity + "%"
+  todayUVIndexEl.textContent = "UV INDEX: " + data.current.uvi + "%"
+  todayIcon.src = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png"
 
-  todayTempEl.textContent = "Temp: " + data.main.temp + "%"
-  todayWindEl.textContent = "Wind: " + data.wind.speed + "MPH"
-  todayHumidityEl.textContent = "Humidity: " + data.main.humidity + "%"
-
-  cityEl.textContent = city.name;
-
+  if(0 < data.current.uvi < 2){
+    todayUVIndexEl.className = "low"
+  }
+  else if(3 > data.current.uvi < 5) {
+    todayUVIndexEl.className = "moderate"
+  }
+  else if(6 > data.current.uvi < 7) {
+    todayUVIndexEl.className = "high"
+  }
+  todayEl.appendChild(todayIcon)
   todayEl.appendChild(todayHumidityEl)
   todayEl.appendChild(todayWindEl)
   todayEl.appendChild(todayTempEl);
-  todayEl.appendChild(cityEl);
-
+  todayEl.appendChild(todayUVIndexEl)
   todaysWeatherContainerEl.appendChild(todayEl);
 }
 
 citySearchForm.addEventListener("submit", formSubmitHandler);
 
-
-
-
 //getWeather()
+var historyInfo = function(name){
+  $("." + name).remove();
+  getCityName(name);
+}
